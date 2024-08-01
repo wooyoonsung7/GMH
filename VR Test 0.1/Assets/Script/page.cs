41e9ddf5714ecc6b6ce41b0d5c887fda97e0087f
wooyoonsung7 <suger8831@gmail.com>
2024-08-01 15:35:24 +0900

    Feat : UI 2page 완성

--- VR Test 0.1/Assets/Script/page.cs
+++ VR Test 0.1/Assets/Script/page.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class page : MonoBehaviour
 {
     public GameObject[] Page = new GameObject[0];
-    public Button page_button;
+    public Button[] Button = new Button[0];
 
     void Start()
     {
         
     }
 
+ 
  
     void Update()
     {
