24c8a6a403f21519b06a5937906c0341f46b6422
wooyoonsung7 <suger8831@gmail.com>
2024-08-01 19:28:21 +0900

    [Feat] 거의 완성

--- VR Test 0.1/Assets/Script/page.cs
+++ VR Test 0.1/Assets/Script/page.cs
@@ -8,17 +8,37 @@ using UnityEngine.XR;
 public class page : MonoBehaviour
 {
     public GameObject[] Page = new GameObject[0];
-    public Button[] Button = new Button[0];
-
-    void Start()
+   
+    public void OnButtonClick1()
     {
-        
+        Page[1].SetActive(true); Page[0].SetActive(false);
     }
-
- 
- 
-    void Update()
+    public void OnButtonClick2()
     {
-            
+        Page[2].SetActive(true); Page[1].SetActive(false);
+    }
+    public void OnButtonClick3()
+    {
+        Page[3].SetActive(true); Page[2].SetActive(false);
+    }
+    public void OnButtonClick4()
+    {
+        Page[4].SetActive(true); Page[3].SetActive(false);
+    }
+    public void OnButtonClick5()
+    {
+        Page[5].SetActive(true); Page[4].SetActive(false);
+    }
+    public void OnButtonClick6()
+    {
+        Page[6].SetActive(true); Page[5].SetActive(false);
+    }
+    public void OnButtonClick7()
+    {
+        Page[7].SetActive(true); Page[6].SetActive(false);
+    }
+    public void OnButtonClick8()
+    {
+        Page[0].SetActive(true); Page[7].SetActive(false);
     }
 }
