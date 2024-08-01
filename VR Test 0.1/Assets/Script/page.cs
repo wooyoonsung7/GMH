700dde2cdf87ba4c8cccc891752dc9fe91c93242
wooyoonsung7 <suger8831@gmail.com>
2024-08-01 15:03:30 +0900

    Feat : UI 1차 완료

---
+++ VR Test 0.1/Assets/Script/page.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class page : MonoBehaviour
+{
+    public GameObject[] Page = new GameObject[0];
+    public Button page_button;
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+            
+    }
+}
